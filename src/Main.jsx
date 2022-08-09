210dff74a2f65b8eb47b089f6abc40ba0ae89f25
John Economou <johnlafayeti@gmail.com>
2022-08-09 10:36:45 -0400

    made server for scraper

--- src/Main.jsx
+++ src/Main.jsx
@@ -5,36 +5,34 @@ import Button from '@mui/material/Button';
 import Chart from 'chart.js/auto'
 import { Line } from 'react-chartjs-2';
 import styled from 'styled-components';
-
-const labels = [
-  'January',
-  'February',
-  'March',
-  'April',
-  'May',
-  'June',
-];
-
-const testData = {
-  labels: labels,
-  datasets: [{
-    label: 'My First dataset',
-    backgroundColor: 'rgb(255, 99, 132)',
-    borderColor: 'rgb(255, 99, 132)',
-    data: [0, 10, 5, 2, 20, 30, 45],
-  }]
-};
+import axios from 'axios';
 
 export default function Main() {
 
-  const [string, setString] = useState('');
+  const [search, setSearch] = useState('');
+  const [sold, setSold] = useState([0, 0, 0, 0, 0, 0]);
+
+  function handleSubmit(){
+    console.log('clicked')
+    axios.get(`http://localhost:4000/scraper/${search}`)
+  }
+
+  const testData = {
+    labels: [0, 1, 2, 3, 4, 5],
+    datasets: [{
+      label: 'Sold Product',
+      backgroundColor: 'rgb(255, 99, 132)',
+      borderColor: 'rgb(255, 99, 132)',
+      data: sold,
+    }]
+  };
 
   return (
     <>
       <SearchBar>
         <Row>
-          <TextField id="outlined-basic" label="Search" variant="outlined" onChange={() => { setString(event.target.value) }} />
-          <Button size="large" variant="contained" style={{ 'marginLeft': '10px', 'height': '55px' }}>Search</Button>
+          <TextField id="outlined-basic" label="Search" variant="outlined" onChange={() => { setSearch(event.target.value) }} />
+          <Button size="large" variant="contained" style={{ 'marginLeft': '10px', 'height': '55px' }} onClick={() => {handleSubmit()}}>Search</Button>
         </Row>
       </SearchBar>
       <Line data={testData} />
