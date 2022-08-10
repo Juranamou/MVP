954f14256b1513aeeb606550f62ea7c821c71e64
John Economou <johnlafayeti@gmail.com>
2022-08-10 13:30:10 -0400

    email notifications setup!

--- server.js
+++ server.js
@@ -5,13 +5,14 @@ var nodemailer = require('nodemailer');
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
+const { response } = require('express');
 
 
 const app = express();
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.json());
 
-app.all('*', function(req, res, next) {
+app.all('*', function (req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Content-Type');
@@ -20,11 +21,11 @@ app.all('*', function(req, res, next) {
 
 app.get('/scraper/:product', async (req, res) => {
   let product = req.params.product;
-  console.log('product',product);
+  console.log('product', product);
 
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
-  await page.goto(`https://www.ebay.com/sch/i.html?_from=R40&_nkw=${product}&_in_kw=1&_ex_kw=&_sacat=0&LH_Sold=1&_udlo=&_udhi=&_samilow=&_samihi=&_sadis=15&_stpos=28117&_sargn=-1%26saslc%3D1&_salic=1&_sop=13&_dmd=1&_ipg=240&LH_Complete=1&_fosrp=1`, {waitUntil: 'networkidle2'});
+  await page.goto(`https://www.ebay.com/sch/i.html?_from=R40&_nkw=${product}&_in_kw=1&_ex_kw=&_sacat=0&LH_Sold=1&_udlo=&_udhi=&_samilow=&_samihi=&_sadis=15&_stpos=28117&_sargn=-1%26saslc%3D1&_salic=1&_sop=13&_dmd=1&_ipg=240&LH_Complete=1&_fosrp=1`, { waitUntil: 'networkidle2' });
 
   let data = await page.evaluate(() => {
     let price = [];
@@ -37,9 +38,9 @@ app.get('/scraper/:product', async (req, res) => {
   })
   console.log('complete');
 
-    // Respond with the image
-    res.send(data);
-    await browser.close();
+  // Respond with the image
+  res.send(data);
+  await browser.close();
 })
 
 app.get('*', (req, res) => {
@@ -49,13 +50,40 @@ app.get('*', (req, res) => {
 app.post('/form', (req, res) => {
   console.log(req.body);
   db.addUser(req.body)
-    .then(() => {res.sendStatus(200);})
-    .catch((err) => {console.log(err);})
+    .then(() => { res.sendStatus(200); })
+    .catch((err) => { console.log(err); })
 });
 
 // qcblmodnpmjvfcyy
 app.post('/email', (req, res) => {
+  var from = req.body.from;
+  var to = req.body.to;
+  var subject = req.body.subject;
+  var message = req.body.message;
 
+  var transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: 'johnlafayeti@gmail.com',
+      pass: 'qcblmodnpmjvfcyy'
+    }
+  });
+
+  var mailOptions = {
+    from: from,
+    to: to,
+    subject: subject,
+    text: message
+  };
+
+  transporter.sendMail(mailOptions, function(error, info){
+    if(error) {
+      console.log(error);
+    } else {
+      console.log('Email sent: ' + info.response);
+    }
+    response.redirect('/')
+  })
 })
 
 app.listen(8080, () => {
--- silentSpy.js
+++ silentSpy.js
@@ -2,40 +2,49 @@ const db = require('./db.js');
 const axios = require('axios');
 require('dotenv').config();
 
-async function looper () {
+async function looper() {
   db.findAll()
-  .then(async (data) => {
-    console.log(data);
-    for (var i = 0; i < data.length; i++) {
-      let user = data[i];
-      let urls = await axiosGet(user.query, user.target, user.min);
-      if (urls.length > 0) {
+    .then(async (data) => {
+      console.log(data);
+      for (var i = 0; i < data.length; i++) {
+        let user = data[i];
+        let urls = await axiosGet(user.query, user.target, user.min);
+        console.log('url', urls);
+        if (urls.length > 0) {
 
+          axios.post('http://localhost:8080/email',
+           {
+            from: 'johnlafayeti@gmail.com',
+            to: user.email,
+            subject: 'wow',
+            message: JSON.stringify(urls)
+          })
+        }
       }
-    }
-  })
+    })
 }
 
-function axiosGet(query, target, min) {
-  axios.get(`https://api.ebay.com/buy/browse/v1/item_summary/search?q=${query}&limit=200`, { headers: { "Authorization": `Bearer ${process.env.TOKEN}` } })
-      .then((data) => {return data.data.itemSummaries; })
-      .then((data) => {
-        let urls = [];
-        for (var i = 0; i < data.length; i++) {
-          let price = data[i].price.value;
-          if (price <= target && price > min) {
-            // console.log(data[i].itemWebUrl);
-            urls.push(data[i].itemWebUrl);
-          }
-        }
-        return urls;
-      })
+async function axiosGet(query, target, min) {
+  let response = await axios.get(`https://api.ebay.com/buy/browse/v1/item_summary/search?q=${query}&limit=200`,
+    { headers: { "Authorization": `Bearer ${process.env.TOKEN}` } })
 
-      .catch((err) => {console.log(err);})
+  let data = response.data.itemSummaries
+  let urls = [];
+
+  for (var i = 0; i < data.length; i++) {
+    let price = data[i].price.value;
+    if (price <= target && price > min) {
+      urls.push(data[i].itemWebUrl);
+    }
+  }
+
+  console.log('URL', urls);
+  return urls;
+
 }
 
 setInterval(() => {
-  // looper()
-}, 5000);
+  looper()
+}, 80000000);
 
 
