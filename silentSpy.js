2e535e137c6b8765cb375a3b365ff1558ade0e19
John Economou <johnlafayeti@gmail.com>
2022-08-10 09:27:50 -0400

    got the urls

--- silentSpy.js
+++ silentSpy.js
@@ -8,16 +8,29 @@ async function looper () {
     console.log(data);
     for (var i = 0; i < data.length; i++) {
       let user = data[i];
-      await axiosGet(user.query);
+      let urls = await axiosGet(user.query, user.target, user.min);
+      if (urls.length > 0) {
+
+      }
     }
   })
 }
 
-function axiosGet(query) {
-  console.log(query);
-  // console.log(process.env.TOKEN);
+function axiosGet(query, target, min) {
   axios.get(`https://api.ebay.com/buy/browse/v1/item_summary/search?q=${query}&limit=200`, { headers: { "Authorization": `Bearer ${process.env.TOKEN}` } })
-      .then((data) => { console.log(data.data.itemSummaries); return data.data.itemSummaries; })
+      .then((data) => {return data.data.itemSummaries; })
+      .then((data) => {
+        let urls = [];
+        for (var i = 0; i < data.length; i++) {
+          let price = data[i].price.value;
+          if (price <= target && price > min) {
+            // console.log(data[i].itemWebUrl);
+            urls.push(data[i].itemWebUrl);
+          }
+        }
+        return urls;
+      })
+
       .catch((err) => {console.log(err);})
 }
 
